ca0cb30f4a0afb75068292ecefb1aad4b45fb3e7
Benhur Mwamba <benhurmt@gmail.com>
2020-12-11 12:18:11 +0200

    Version 1

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-//Import Contractconst DStorage = artifacts.require("DStorage");
+const DStorage = artifacts.require("DStorage");
 
 module.exports = function(deployer) {
+
+	deployer.deploy(DStorage);
 	//Deploy Contract
 };
